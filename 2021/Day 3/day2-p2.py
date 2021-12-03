68d9b5872f05a5a59b6bce5f4925a95355042da9
Clayton-Stuart <72456578+Clayton-Stuart@users.noreply.github.com>
2021-12-03 09:44:58 -0500

    Add files via upload

---
+++ 2021/Day 3/day2-p2.py
@@ -0,0 +1,24 @@
+def main(file):
+    instructions = open(file, 'r')
+    ls = instructions.readlines()
+    instructions.close()
+    for i in range(len(ls)):
+        ls[i] = ls[i].split(' ')
+        ls[i][1] = int(ls[i][1])
+        
+    depth = 0
+    horizontal = 0
+    aim = 0
+
+    for i in range(len(ls)):
+        if ls[i][0] == 'forward':
+            horizontal += ls[i][1]
+            depth += ls[i][1] * aim
+        elif ls[i][0] == 'up':
+            aim -= ls[i][1]
+        elif ls[i][0] == 'down':
+            aim += ls[i][1]
+
+    return depth * horizontal
+
+print(main("input.txt"))
