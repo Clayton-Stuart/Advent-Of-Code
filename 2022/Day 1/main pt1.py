3d156e64c477a09e79010c5d7ccdcbc7f957aa5e
Clayton-Stuart <72456578+Clayton-Stuart@users.noreply.github.com>
2022-12-02 09:11:22 -0500

    Add files via upload

---
+++ 2022/Day 1/main pt1.py
@@ -0,0 +1,34 @@
+file = open('input.txt', 'r')
+lines = file.readlines()
+file.close()
+
+elves = [0]
+for i in lines:
+    if i != '\n':
+        elves[-1] += int(i.strip())
+    else:
+        elves.append(0)
+
+def part1(elves):
+    largest = elves[0]
+    for i in range(1, len(elves)):
+        if elves[i] > largest:
+            largest = elves[i]
+    print(largest)
+
+def part2(elves):
+    top = []
+    for _ in range(3):
+        largest = elves[0]
+        index = 0
+        for i in range(1, len(elves)):
+            if elves[i] > largest:
+                largest = elves[i]
+                index = i
+        top.append(elves[index])
+        elves.pop(index)
+    print(sum(top))
+
+part2(elves)
+
+            
