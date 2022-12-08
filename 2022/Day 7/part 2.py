3dd3e5b2c5f9d01b16b32e83443a63e3f649caf9
Clayton-Stuart <72456578+Clayton-Stuart@users.noreply.github.com>
2022-12-08 12:13:38 -0500

    Add files via upload

---
+++ 2022/Day 7/part 2.py
@@ -0,0 +1,58 @@
+file = open('input.txt', 'r')
+ls = file.readlines()
+file.close()
+
+digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+current_path = []
+used_names = []
+current_directory = '/'
+limit = 100000
+total = 70000000
+needed = 30000000
+directories = {'/': 0}
+
+
+# Strip new line characters
+for i in range(len(ls)):
+    ls[i] = ls[i].strip()
+
+for i in ls:
+    if i.split(' ')[0] == 'dir':
+        directories[i.split(' ')[1]] = 0
+
+
+
+for i in range(len(ls)):
+    command = ls[i].split(' ')
+
+    # check if the line is a command
+    if command[0] == '$':
+        if command[1] == 'cd':
+            if command[2] == '..':
+                current_path.pop()
+
+
+            elif command[2] not in used_names:
+                used_names.append(command[2])
+                current_path.append(command[2])
+            else:
+                used_names.append(command[2] + str(i))
+                current_path.append(command[2] + str(i))
+                directories[command[2] + str(i)] = 0
+
+    # The line is an output
+    else:
+        if ls[i][0] in digits:
+            for e in current_path:
+                directories[e] += int(command[0])
+
+space = total - directories['/']
+
+sizes = []
+for key in directories:
+    if directories[key] >= needed - space:
+        sizes.append(directories[key])
+        
+sizes.sort()
+print(sizes[0])
+                
